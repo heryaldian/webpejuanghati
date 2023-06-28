cb8beb09c9ea64cf4d4a8c959d21ac11f97440e3
unknown <heryvolt10@gmail.com>
2023-06-28 22:22:09 +0700

    tambahan menu product

--- js/allsetting.js
+++ js/allsetting.js
@@ -1,19 +1,35 @@
 // ===== NAVBAR START =====
-// toggle Navbar
+// toggle Navbar listmenu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // menu di click
-document.querySelector("#list-menu").onclick = () => {
+document.querySelector("#list-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // click diluar sidebar untuk hilngkan list menu
 const listMenu = document.querySelector("#list-menu");
+const searchButton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!listMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
+
+// toggle Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+// menu di click
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
 // ===== NAVBAR END =====
